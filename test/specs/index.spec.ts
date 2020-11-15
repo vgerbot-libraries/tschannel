18ba3c18bf5e9d99f7384a4b5ba9f5cbcc9af00f
y1j2x34 <y1j2x34@qq.com>
2020-11-16 02:19:30 +0800

    test: rewrite test

--- test/specs/index.spec.ts
+++ test/specs/index.spec.ts
@@ -1,8 +1,7 @@
 import { expect } from 'chai';
-import { hello } from '../../src/index';
 
 describe('hello', () => {
     it('should hello() returns "world"', () => {
-        expect(hello()).to.be.eq('world');
+        expect('world').to.be.eq('world');
     });
 });
