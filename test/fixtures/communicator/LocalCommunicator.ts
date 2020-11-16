a09eedeedcda7c07fea3d923506ea9a2622a3540
y1j2x34 <y1j2x34@qq.com>
2020-11-16 23:04:23 +0800

    test: RMI test case

---
+++ test/fixtures/communicator/LocalCommunicator.ts
@@ -0,0 +1,27 @@
+import AbstractCommunicator from '../../../src/communicators/AbstractCommunicator';
+import { Communicator, CommunicatorMessageReceiver } from '../../../src/types/Communicator';
+import { InvokeMethodData } from '../../../src/types/InvokeMethodData';
+import Payload from '../../../src/types/Payload';
+import { Returning } from '../../../src/types/Returning';
+import { SerializableValue } from '../../../src/types/Serializable';
+
+export default class LocalCommunicator extends AbstractCommunicator implements Communicator {
+    public messageReceivers: CommunicatorMessageReceiver[] = [];
+    private other?: LocalCommunicator;
+    connectTo(other: LocalCommunicator) {
+        this.other = other;
+        if (other.other !== this) {
+            other.connectTo(this);
+        }
+    }
+    send(payload: Payload<SerializableValue>): void {
+        this.other?.messageReceivers.forEach(receiver => {
+            receiver(payload.serialize() as InvokeMethodData | Returning);
+        });
+    }
+    close(): void {
+        this.messageReceivers = [];
+        this.other?.close();
+        this.other = undefined;
+    }
+}
--- test/specs/index.spec.ts
+++ test/specs/index.spec.ts
@@ -1,7 +1,40 @@
-import { expect } from 'chai';
+import { RMI } from '../../src';
+import { rclass } from '../../src/annotations/rclass';
+import { rmethod } from '../../src/annotations/rmethod';
+import LocalCommunicator from '../fixtures/communicator/LocalCommunicator';
 
-describe('hello', () => {
-    it('should hello() returns "world"', () => {
-        expect('world').to.be.eq('world');
+describe('RMI', () => {
+    it('should lclass works correctly', () => {
+        const communicatorA = new LocalCommunicator();
+        const communicatorB = new LocalCommunicator();
+        communicatorA.connectTo(communicatorB);
+
+        const rmiA = new RMI('local', communicatorA);
+        const rmiB = new RMI('local', communicatorB);
+
+        interface MD5Calculator {
+            computeMD5(str: string): string;
+        }
+        @rclass({ id: 'remote-class' })
+        class RemoteMD5Calculator implements MD5Calculator {
+            constructor() {
+                console.log();
+            }
+            @rmethod({})
+            computeMD5(_: string): string {
+                return '';
+            }
+        }
+
+        class MD5CalculatorImpl implements MD5Calculator {
+            computeMD5(str: string): string {
+                return 'md5:' + str;
+            }
+        }
+        rmiB.lclass('remote-class', MD5CalculatorImpl);
+        const RClass = rmiA.rclass(RemoteMD5Calculator);
+
+        const value = new RClass().computeMD5('123456');
+        console.info(value);
     });
 });
