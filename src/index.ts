995fda47247a62a12604abb1927dc88bdef4a41c
y1j2x34 <y1j2x34@qq.com>
2020-11-22 21:24:42 +0800

    feat: exports ParameterType

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { rmethod } from './annotations/rmethod';
 export { rclass } from './annotations/rclass';
 export { RMI } from './RMI';
+export { ParameterType } from './types/ParameterType';
