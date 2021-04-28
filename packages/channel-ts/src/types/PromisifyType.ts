3e57139788a12e8e46397e1cfec690f8870d01fa
y1j2x34 <y1j2x34@qq.com>
2021-04-28 23:25:05 +0800

    fix: PromisifyClass

--- packages/channel-ts/src/types/PromisifyType.ts
+++ packages/channel-ts/src/types/PromisifyType.ts
@@ -1,9 +1,15 @@
 import { Constructor } from './AnyConstructor';
 
-export type PromisifyClass<T, K extends keyof T = keyof T> = Constructor<
-    {
-        [key in K]: T[K] extends (...args) => unknown
-            ? (...args: Parameters<T[K]>) => Promise<ReturnType<T[K]> extends Promise<infer P> ? P : ReturnType<T[K]>>
-            : T[K];
-    }
->;
+export type PromisifyFunction<F> = F extends (...args: infer P) => infer R
+    ? (...args: P) => Promise<R extends Promise<infer P> ? P : R>
+    : F;
+
+type FilterOutAttributes<Base> = {
+    [Key in keyof Base]: Base[Key] extends (...args) => unknown ? Base[Key] : never;
+};
+
+export type PromisifyClass<P> = Constructor<PromisifyObject<P>>;
+
+export type PromisifyObject<P, T extends { [key: string]: (...args) => unknown } = FilterOutAttributes<P>> = {
+    [Key in keyof T]: ReturnType<T[Key]> extends Promise<unknown> ? T[Key] : PromisifyFunction<T[Key]>;
+};
