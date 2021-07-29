14c547e8dc4002a20efdf5c29585452955dbb51b
y1j2x34 <y1j2x34@qq.com>
2021-07-30 01:43:07 +0800

    refactor: rewrite ts transformer

--- packages/channel-ts-transformer/lib/index.js
+++ packages/channel-ts-transformer/lib/index.js
@@ -3,9 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const typescript_1 = tslib_1.__importDefault(require("typescript"));
 class ChannelProgramContext {
-    constructor() {
-        this.channelInstanceDeclarations = [];
-    }
     isChannelType(node) {
     }
 }
@@ -33,29 +30,10 @@ function transformer(program, options) {
 }
 exports.default = transformer;
 function visitNode(node, program, programCtx, context, options) {
-    var _a, _b;
+    var _a, _b, _c;
     const typeChecker = program.getTypeChecker();
     const factory = context.factory;
-    if (typescript_1.default.isPropertyAssignment(node)) {
-        resolveInitializerExpression(node, typeChecker, programCtx);
-    }
-    else if ((typescript_1.default.isParameter(node) || typescript_1.default.isPropertyDeclaration(node) || typescript_1.default.isVariableDeclaration(node))) {
-        const type = node.type;
-        if (type) {
-            const typeSymbol = typeChecker.getSymbolAtLocation(type.typeName);
-            if (typeSymbol === programCtx.channelClassSymbol) {
-                const variableSymbol = typeChecker.getSymbolAtLocation(node.name);
-                if (!variableSymbol) {
-                    return;
-                }
-                programCtx.channelInstanceDeclarations.push(variableSymbol.valueDeclaration);
-            }
-        }
-        else if (node.initializer) {
-            resolveInitializerExpression(node, typeChecker, programCtx);
-        }
-    }
-    else if (typescript_1.default.isImportDeclaration(node)) {
+    if (typescript_1.default.isImportDeclaration(node)) {
         if (!typescript_1.default.isStringLiteral(node.moduleSpecifier)) {
             return;
         }
@@ -75,8 +53,10 @@ function visitNode(node, program, programCtx, context, options) {
             if (!channelClassDeclaration) {
                 return;
             }
-            programCtx.channelClassDeclaration = channelClassDeclaration;
-            programCtx.channelClassSymbol = typeChecker.getSymbolAtLocation(channelClassDeclaration.name);
+            const channelClassType = typeChecker.getTypeAtLocation(channelClassDeclaration);
+            const channelClassSymbol = channelClassType.getSymbol();
+            programCtx.channelClassSymbol = channelClassSymbol;
+            programCtx.rclassSymbol = (_b = channelClassSymbol === null || channelClassSymbol === void 0 ? void 0 : channelClassSymbol.members) === null || _b === void 0 ? void 0 : _b.get('rclass');
         }
     }
     else if (typescript_1.default.isCallExpression(node)) {
@@ -86,22 +66,13 @@ function visitNode(node, program, programCtx, context, options) {
             if (propertyName !== 'rclass') {
                 return;
             }
-            const ownerExpression = propertyExpression.expression;
-            if (typescript_1.default.isNewExpression(ownerExpression)) {
-                const classSymbol = typeChecker.getSymbolAtLocation(ownerExpression.expression);
-                if (classSymbol !== programCtx.channelClassSymbol) {
-                    return;
-                }
+            const propertyType = typeChecker.getTypeAtLocation(propertyExpression);
+            if (!propertyType) {
+                return;
             }
-            else {
-                const symbol = typeChecker.getSymbolAtLocation(ownerExpression);
-                if (!symbol) {
-                    return;
-                }
-                const index = programCtx.channelInstanceDeclarations.indexOf(symbol.valueDeclaration);
-                if (index < 0) {
-                    return;
-                }
+            const propertySymbol = propertyType.getSymbol();
+            if (!propertySymbol || propertySymbol !== programCtx.rclassSymbol) {
+                return;
             }
             const typeArgs = node.typeArguments;
             if (!typeArgs || typeArgs.length < 1) {
@@ -109,7 +80,7 @@ function visitNode(node, program, programCtx, context, options) {
             }
             const typeNode = typeArgs[0];
             const typeNodeObj = typeChecker.getTypeFromTypeNode(typeNode);
-            const declarations = (_b = typeNodeObj.getSymbol()) === null || _b === void 0 ? void 0 : _b.getDeclarations();
+            const declarations = (_c = typeNodeObj.getSymbol()) === null || _c === void 0 ? void 0 : _c.getDeclarations();
             if (!declarations || declarations.length < 1) {
                 return;
             }
@@ -146,97 +117,5 @@ function createRemoteClassExpression(typeNode, typeChecker, factory) {
     });
     const classExpression = factory.createClassExpression([], [], className, [], [typescript_1.default.factory.createHeritageClause(typescript_1.default.SyntaxKind.ImplementsKeyword, [])], classMembers);
     return classExpression;
-}
-function resolveInitializerExpression(node, typeChecker, programCtx) {
-    var _a;
-    const errorType = typeChecker.getTypeAtLocation(undefined);
-    const type = typeChecker.getTypeAtLocation(node);
-    if (type !== errorType) {
-        const aliasSymbol = type.aliasSymbol;
-        let isTypeMatch = false;
-        if (aliasSymbol) {
-            const intersectionDeclarations = (_a = aliasSymbol.declarations) === null || _a === void 0 ? void 0 : _a.filter(it => typescript_1.default.isInterfaceDeclaration(it));
-            if (!intersectionDeclarations) {
-                return;
-            }
-            isTypeMatch = intersectionDeclarations.some(it => {
-                if (!typescript_1.default.isTypeAliasDeclaration(it)) {
-                    return;
-                }
-                if (!typescript_1.default.isIntersectionTypeNode(it.type)) {
-                    return;
-                }
-                const types = it.type.types;
-                return types.some(it => {
-                    if (!typescript_1.default.isTypeReferenceNode(it)) {
-                        return;
-                    }
-                    const symbol = typeChecker.getSymbolAtLocation(it.typeName);
-                    if (programCtx.channelClassSymbol === symbol) {
-                        return true;
-                    }
-                });
-            });
-        }
-        else if (type.getSymbol() === programCtx.channelClassSymbol) {
-            isTypeMatch = true;
-        }
-        if (isTypeMatch) {
-            const variableSymbol = typeChecker.getSymbolAtLocation(node.name);
-            const nodeDeclarations = variableSymbol === null || variableSymbol === void 0 ? void 0 : variableSymbol.getDeclarations();
-            if (!nodeDeclarations) {
-                return;
-            }
-            programCtx.channelInstanceDeclarations.push(...nodeDeclarations);
-            return;
-        }
-    }
-    const initializer = node.initializer;
-    if (!initializer) {
-        return;
-    }
-    if (typescript_1.default.isNewExpression(initializer)) {
-        const typeSymbol = typeChecker.getSymbolAtLocation(initializer.expression);
-        if (typeSymbol === programCtx.channelClassSymbol) {
-            const variableSymbol = typeChecker.getSymbolAtLocation(node.name);
-            if (!variableSymbol) {
-                return;
-            }
-            programCtx.channelInstanceDeclarations.push(variableSymbol.valueDeclaration);
-        }
-    }
-    else if (typescript_1.default.isIdentifier(initializer)) {
-        const initializerSymbol = typeChecker.getSymbolAtLocation(initializer);
-        if (!initializerSymbol) {
-            return;
-        }
-        const declarations = initializerSymbol.getDeclarations();
-        if (!declarations || declarations.length < 1) {
-            return;
-        }
-        const declaration = declarations[0];
-        if (programCtx.channelInstanceDeclarations.indexOf(declaration) === -1) {
-            return;
-        }
-        const nodeSymbol = typeChecker.getSymbolAtLocation(node.name);
-        const nodeDeclarations = nodeSymbol === null || nodeSymbol === void 0 ? void 0 : nodeSymbol.getDeclarations();
-        if (!nodeDeclarations) {
-            return;
-        }
-        programCtx.channelInstanceDeclarations.push(...nodeDeclarations);
-    }
-    else if (typescript_1.default.isAsExpression(initializer)) {
-        const typeNode = initializer.type;
-        const classSymbol = typeChecker.getSymbolAtLocation(typeNode.typeName);
-        if (programCtx.channelClassSymbol !== classSymbol) {
-            return;
-        }
-        const nodeSymbol = typeChecker.getSymbolAtLocation(node.name);
-        const nodeDeclarations = nodeSymbol === null || nodeSymbol === void 0 ? void 0 : nodeSymbol.getDeclarations();
-        if (!nodeDeclarations) {
-            return;
-        }
-        programCtx.channelInstanceDeclarations.push(...nodeDeclarations);
-    }
 }
 //# sourceMappingURL=index.js.map
--- packages/channel-ts-transformer/src/index.ts
+++ packages/channel-ts-transformer/src/index.ts
@@ -1,9 +1,8 @@
 import ts from 'typescript';
 
 class ChannelProgramContext {
-    public channelClassDeclaration?: ts.ImportSpecifier;
     public channelClassSymbol?: ts.Symbol;
-    public channelInstanceDeclarations: Array<ts.Declaration> = [];
+    public rclassSymbol?: ts.Symbol;
     public sourceFileNode!: ts.SourceFile;
     public isChannelType(node: ts.Node) {
 
@@ -51,23 +50,7 @@ function visitNode(
 ): undefined | ts.Node | Array<ts.Node> {
     const typeChecker = program.getTypeChecker();
     const factory = context.factory;
-    if(ts.isPropertyAssignment(node)) {
-        resolveInitializerExpression(node, typeChecker, programCtx);
-    } else if((ts.isParameter(node) || ts.isPropertyDeclaration(node) || ts.isVariableDeclaration(node))) {
-        const type = node.type as ts.TypeReferenceNode | null;
-        if(type) {
-            const typeSymbol = typeChecker.getSymbolAtLocation(type.typeName);
-            if(typeSymbol === programCtx.channelClassSymbol) {
-                const variableSymbol = typeChecker.getSymbolAtLocation(node.name);
-                if(!variableSymbol) {
-                    return;
-                }
-                programCtx.channelInstanceDeclarations.push(variableSymbol.valueDeclaration);
-            }
-        } else if(node.initializer) {
-            resolveInitializerExpression(node, typeChecker, programCtx);
-        }
-    } else if (ts.isImportDeclaration(node)) {
+    if (ts.isImportDeclaration(node)) {
         if (!ts.isStringLiteral(node.moduleSpecifier)) {
             return;
         }
@@ -87,10 +70,10 @@ function visitNode(
             if (!channelClassDeclaration) {
                 return;
             }
-            programCtx.channelClassDeclaration = channelClassDeclaration;
-            programCtx.channelClassSymbol = typeChecker.getSymbolAtLocation(
-                channelClassDeclaration.name
-            );
+            const channelClassType = typeChecker.getTypeAtLocation(channelClassDeclaration);
+            const channelClassSymbol = channelClassType.getSymbol();
+            programCtx.channelClassSymbol = channelClassSymbol;
+            programCtx.rclassSymbol = channelClassSymbol?.members?.get('rclass' as ts.__String);
         }
     } else if (ts.isCallExpression(node)) {
         const propertyExpression = node.expression;
@@ -99,21 +82,13 @@ function visitNode(
             if(propertyName !== 'rclass') {
                 return;
             }
-            const ownerExpression = propertyExpression.expression;
-            if(ts.isNewExpression(ownerExpression)) {
-                const classSymbol = typeChecker.getSymbolAtLocation(ownerExpression.expression);
-                if (classSymbol !== programCtx.channelClassSymbol) {
-                    return;
-                }
-            } else {
-                const symbol = typeChecker.getSymbolAtLocation(ownerExpression);
-                if (!symbol) {
-                    return;
-                }
-                const index = programCtx.channelInstanceDeclarations.indexOf(symbol.valueDeclaration);
-                if (index < 0) {
-                    return;
-                }
+            const propertyType = typeChecker.getTypeAtLocation(propertyExpression);
+            if(!propertyType) {
+                return;
+            }
+            const propertySymbol = propertyType.getSymbol();
+            if(!propertySymbol || propertySymbol !== programCtx.rclassSymbol) {
+                return;
             }
             const typeArgs = node.typeArguments;
             if (!typeArgs || typeArgs.length < 1) {
@@ -176,97 +151,4 @@ function createRemoteClassExpression(typeNode: ts.TypeNode, typeChecker: ts.Type
         classMembers
     );
     return classExpression;
-}
-
-function resolveInitializerExpression(
-    node: ts.PropertyAssignment | ts.ParameterDeclaration | ts.PropertyDeclaration | ts.VariableDeclaration,
-    typeChecker: ts.TypeChecker,
-    programCtx: ChannelProgramContext
-) {
-    const errorType = typeChecker.getTypeAtLocation(undefined as unknown as ts.Node);
-    const type = typeChecker.getTypeAtLocation(node);
-    if(type !== errorType) {
-        const aliasSymbol = type.aliasSymbol;
-        let isTypeMatch = false;
-        if(aliasSymbol) {
-            const intersectionDeclarations = aliasSymbol.declarations?.filter(it => ts.isInterfaceDeclaration(it));
-            if(!intersectionDeclarations) {
-                return;
-            }
-            isTypeMatch = intersectionDeclarations.some(it => {
-                if(!ts.isTypeAliasDeclaration(it)) {
-                    return;
-                }
-                if(!ts.isIntersectionTypeNode(it.type)) {
-                    return;
-                }
-                const types = it.type.types;
-                return types.some(it => {
-                    if(!ts.isTypeReferenceNode(it)) {
-                        return;
-                    }
-                    const symbol = typeChecker.getSymbolAtLocation(it.typeName);
-                    if(programCtx.channelClassSymbol === symbol) {
-                        return true;
-                    }
-                })
-            });
-        } else if(type.getSymbol() === programCtx.channelClassSymbol) {
-            isTypeMatch = true;
-        }
-        if(isTypeMatch) {
-            const variableSymbol = typeChecker.getSymbolAtLocation(node.name);
-            const nodeDeclarations = variableSymbol?.getDeclarations();
-            if(!nodeDeclarations) {
-                return;
-            }
-            programCtx.channelInstanceDeclarations.push(...nodeDeclarations);
-            return;
-        }
-    }
-    const initializer = node.initializer;
-    if(!initializer) {
-        return;
-    }
-    if(ts.isNewExpression(initializer)) {
-        const typeSymbol = typeChecker.getSymbolAtLocation(initializer.expression);
-        if(typeSymbol === programCtx.channelClassSymbol) {
-            const variableSymbol = typeChecker.getSymbolAtLocation(node.name);
-            if(!variableSymbol) {
-                return;
-            }
-            programCtx.channelInstanceDeclarations.push(variableSymbol.valueDeclaration);
-        }
-    } else if(ts.isIdentifier(initializer)) {
-        const initializerSymbol = typeChecker.getSymbolAtLocation(initializer);
-        if(!initializerSymbol) {
-            return;
-        }
-        const declarations = initializerSymbol.getDeclarations();
-        if(!declarations || declarations.length < 1) {
-            return;
-        }
-        const declaration = declarations[0];
-        if(programCtx.channelInstanceDeclarations.indexOf(declaration) === -1) {
-            return;
-        }
-        const nodeSymbol = typeChecker.getSymbolAtLocation(node.name);
-        const nodeDeclarations = nodeSymbol?.getDeclarations();
-        if(!nodeDeclarations) {
-            return;
-        }
-        programCtx.channelInstanceDeclarations.push(...nodeDeclarations);
-    } else if(ts.isAsExpression(initializer)) {
-        const typeNode = initializer.type as ts.TypeReferenceNode;
-        const classSymbol = typeChecker.getSymbolAtLocation(typeNode.typeName);
-        if(programCtx.channelClassSymbol !== classSymbol) {
-            return;
-        }
-        const nodeSymbol = typeChecker.getSymbolAtLocation(node.name);
-        const nodeDeclarations = nodeSymbol?.getDeclarations();
-        if(!nodeDeclarations) {
-            return;
-        }
-        programCtx.channelInstanceDeclarations.push(...nodeDeclarations);
-    }
 }
