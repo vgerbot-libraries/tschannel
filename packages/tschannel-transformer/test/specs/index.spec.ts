8fa3db653e8881004eadaa9d98e2a314257e50e3
y1j2x34 <y1j2x34@qq.com>
2021-09-11 14:44:42 +0800

    test: more test cases

--- packages/tschannel-transformer/test/specs/index.spec.ts
+++ packages/tschannel-transformer/test/specs/index.spec.ts
@@ -2,7 +2,7 @@ import { transpile } from '../common/compiler';
 import transformer from '../../src';
 
 describe('@tschannel/transformer', () => {
-    it('Should run normally with the transformer', async () => {
+    it('should run normally with the transformer', async () => {
         const source = `
             import { Channel } from '@tschannel/core';
             export let x: string = 'string';
@@ -13,12 +13,69 @@ describe('@tschannel/transformer', () => {
         });
         expect(output).toMatchSnapshot();
     });
-    it('Should transform the interface parameter correctly', () => {
+    it('should transform the interface parameter correctly', () => {
         const source = `
             import { Channel } from '@tschannel/core';
             const channel = new Channel();
             channel.rclass<RemoteAPI>();
             interface RemoteAPI {
+                method(){}
+            }
+        `;
+        const output = transpile(source, {
+            channelTransformer: transformer
+        });
+        expect(output).toMatchSnapshot();
+    });
+    it('should be able to detect the module alias', () => {
+        const source = `
+            import { Channel as ChannelAlias } from '@tschannel/core';
+            const channel = new Channel();
+            channel.rclass<RemoteAPI>();
+            interface RemoteAPI {
+                method(){}
+            }
+        `;
+        const output = transpile(source, {
+            channelTransformer: transformer
+        });
+        expect(output).toMatchSnapshot();
+    });
+    it('should be able to exclude methods calling that has more than 2 arguments', () => {
+        const source = `
+            import { Channel as ChannelAlias } from '@tschannel/core';
+            const channel = new Channel();
+            channel.rclass<RemoteAPI>('RemoteAPI1', class {
+                method(){}
+            });
+            interface RemoteAPI {
+                method(){}
+            }
+        `;
+        const output = transpile(source, {
+            channelTransformer: transformer
+        });
+        expect(output).toMatchSnapshot();
+    })
+    it('should be able to exclude methods without type arguments', () => {
+        const source = `
+            import { Channel } from '@tschannel/core';
+            const channel = new Channel();
+            channel.rclass('RemoteAPI', class RemoteAPI {
+                method(){}
+            });
+        `;
+        const output = transpile(source, {
+            channelTransformer: transformer
+        });
+        expect(output).toMatchSnapshot();
+    });
+    it('should be able to reference the class', () => {
+        const source = `
+            import { Channel } from '@tschannel/core';
+            const channel = new Channel();
+            channel.rclass<RemoteAPI>();
+            class RemoteAPI {
                 method(){}
             }
         `;
