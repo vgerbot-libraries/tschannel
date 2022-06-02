e6cb54abab707937b59002b80839900cc7fe8bdc
y1j2x34 <y1j2x34@qq.com>
2022-06-02 23:06:08 +0800

    chore: update header-max-length to 100

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
     rules: {
         'body-leading-blank': [1, 'always'],
         'footer-leading-blank': [1, 'always'],
-        'header-max-length': [2, 'always', 72],
+        'header-max-length': [2, 'always', 100],
         'scope-case': [2, 'always', 'lowerCase'],
         'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'never', '.'],
